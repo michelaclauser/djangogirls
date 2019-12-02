edbd75e7cdf1283005c839ca02114da7e0659fed
michela clauser <michelaclauser@gmail.com>
2019-12-02 14:43:01 +0100

    Modified templates to display posts from database.

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.urls import path, include
 from django.shortcuts import render
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+from django.contrib import admin 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
